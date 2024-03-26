53337455f384789ec1f2f687c168fbd9e05d2012
W-xzg <42465420+W-xzg@users.noreply.github.com>
2024-03-26 22:16:36 +0800

    0326

--- c++/test.cpp
+++ c++/test.cpp
@@ -10,3 +10,25 @@ int main()
 }
 */
 //三月26号
+//b2002
+/*
+#include <cstdio>
+int main()
+{
+    printf("Hello,World!");
+    return 0;
+}
+*/
+//p1421
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,ab;
+    cin>>a>>b;
+    ab=(a*10+b)/19;
+    cout<<ab<<endl;
+    return 0;
+}
+*/
